af4561d8540d2622515a160c3eb9e09b2035c863
Scott Prue <scott.prue@side.com>
2025-09-03 13:09:18 -0600

    chore: add ts version

--- jest.config.ts
+++ jest.config.ts
@@ -1,10 +1,11 @@
-const nextJest = require('next/jest');
+import type { Config } from 'jest'
+import nextJest from 'next/jest.js'
 
 const createJestConfig = nextJest({
   dir: './',
 });
 
-const customJestConfig = {
+const config: Config = {
   // Ignore transforming all node_modules except @faker-js/faker since it is ESM
   transformIgnorePatterns: [
     '/node_modules/(?!@faker-js\\/faker)/'
@@ -12,15 +13,15 @@ const customJestConfig = {
 };
 
 // Create the Jest config and then override transformIgnorePatterns to ensure it's not overridden by Next.js
-const jestConfig = createJestConfig(customJestConfig);
+const jestConfig = createJestConfig(config);
 
-module.exports = jestConfig;
+// export default jestConfig;
 
 // Override transformIgnorePatterns like below works - proving Next.js's setting of the config is not working
-// module.exports = async (...args) => {
-//   const config = await jestConfig(...args);
-//   config.transformIgnorePatterns = [
-//     '/node_modules/(?!(swr-openapi|change-case|@faker-js\\/faker)/)'
-//   ];
-//   return config;
-// };
+export default async () => {
+  const newConfig = await jestConfig();
+  newConfig.transformIgnorePatterns = [
+    '/node_modules/(?!(swr-openapi|change-case|@faker-js\\/faker)/)'
+  ];
+  return newConfig;
+};
